9dc8c03daf5a391409776ddbe5a73e6be1a28db0
vanessaza06 <yurivanesaaza@gmail.com>
2026-09-02 17:10:58 -0500

    .

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from app.views import *
 urlpatterns = [
     # Definirás tus urls aquí, por ejemplo:
     # path('', views.home, name='home'),
-    path('bodega/', include('app.views.bodega.urls'))
-  
+    path('bodega/', include('app.views.bodega.urls')),
+    path('proveedores/', include('app.views.proveedores.urls')),
+    path('devoluciones/', include('app.views.devoluciones.urls')),
+    path('compra/', include('app.views.compra.urls')),
 ]
